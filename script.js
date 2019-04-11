903b39fdb460489c0b241727e4307cd5cc27c551
vyang16 <43174639+vyang16@users.noreply.github.com>
2019-04-11 16:09:05 +0800

    finished

--- script.js
+++ script.js
@@ -4,13 +4,60 @@ function computeCost(){
   var banana = document.getElementById("banana").value;
   var orange = document.getElementById("orange").value;
   //Check if variables are valid or not
-    document.getElementById('cost').value = apple * 0.69 + banana * 0.39 + orange * 0.59 ;
+  var num = apple * 0.69 + banana * 0.39 + orange * 0.59;
+  document.getElementById('cost').value = parseFloat(Math.round(num * 100) / 100).toFixed(2);;
 }
 
 function checkValid(val, id) {
   var pos  = val.search(/^\d+$/);
   if(pos != 0){
     alert("Your input [" + val + "] in " + id + " is not valid. Please change it to a valid quantity.");
+    document.getElementById(id).value = 0;
+    computeCost();
+  }else{
+    computeCost();
   }
-
 }
+
+function validateForm() {
+  var x = document.forms["order"]["name"].value;
+  var payment = document.forms["order"]["payment"].value;
+  var apple = document.getElementById("apple").value;
+  var banana = document.getElementById("banana").value;
+  var orange = document.getElementById("orange").value;
+
+  if (x == "") {
+    alert("Name must be filled out");
+    return false;
+  }
+  if (payment == "") {
+    alert("Please choose a payment option");
+    return false;
+  }
+  if(apple==0 && banana==0 && orange==0){
+    alert("Please make a valid order");
+    return false;
+  }
+}
+
+/*
+function howManyWordsInInput(id)
+{
+    var text = document.getElementById(id).value;
+    text = text.split(" ");
+    for(word in text){
+      var pos = word.search(\w+);
+      if(pos != 0){
+        alert("Please put in a valid name");
+        return -1;
+      }
+    }
+    return text.length;
+}
+
+function checkName(id){
+  if(howManyWordsInInput(id) < 2){
+    alert("Please put in your whole name!"); //at least two words, can have more
+  }
+}
+*/
--- server.php
+++ server.php
@@ -1,33 +1,106 @@
-<!--Save php file in /var/www/html/server.php -->
-<!--To run or test php file, start apache with
-1. sudo systemctl restart apache2
-2. Type in browser http://192.168.0.102/server.php
-
-## Start command ##
-systemctl start apache2.service
-## Stop command ##
-systemctl stop apache2.service
-## Restart command ##
-systemctl restart apache2.service
-
-
--->
-
-<!DOCTYPE html>
-<html>
-<body>
-
 <?php
-$txt = "Hello world!";
-$x = 5;
-$y = 10.5;
+//Read Variables
+$name = $_POST['name'];
+$payment = $_POST['payment'];
+$apple = $_POST['apple'];
+$orange = $_POST['orange'];
+$banana = $_POST['banana'];
 
-echo $txt;
-echo "<br>";
-echo $x;
-echo "<br>";
-echo $y;
+
+$totalCost = number_format((($apple * 0.69) + ($orange * 0.59) + ($banana * 0.39)), 2); //only show two decimals
+
+//store order in order.txt
+$file = 'order.txt';
+// Creates file if it does not exist yet
+if(!is_file($file)){
+    $contents = "Total number of apples: 0\r\nTotal number of oranges: 0\r\nTotal number of bananas: 0\r\n";
+    file_put_contents($file, $contents);
+}
+$order_file = fopen("order.txt", "r+") or die("Unable to create file!");
+$data = fread($order_file, filesize($file));
+// Uses regex to finds the counts of each fruits within the file
+preg_match_all("/[0-9]+/", $data, $fruit_counts);
+$old_apple_count = $fruit_counts[0][0];
+$old_orange_count = $fruit_counts[0][1];
+$old_banana_count = $fruit_counts[0][2];
+$new_apple_count = $old_apple_count + $apple;
+$new_orange_count = $old_orange_count + $orange;
+$new_banana_count = $old_banana_count + $banana;
+$new_contents = "Total number of apples: $new_apple_count\r\nTotal number of oranges: $new_orange_count\r\nTotal number of bananas: $new_banana_count\r\n";
+file_put_contents($file, $new_contents);
+//Closes file (good practice)
+fclose($order_file);
 ?>
 
+<!DOCTYPE html>
+<html xmlns = "http://www.w3.org/1999/xhtml">
+  <head>
+    <meta charset="utf-8">
+    <link rel="icon" href="image/fruit_bowl-512.png">
+    <link rel="stylesheet" href="style.css">
+    <title>Fruit Shop</title>
+
+
+    <style type="text/css">
+      body{
+        font-family: "Courier New", Courier, "Lucida Sans Typewriter", "Lucida Typewriter", monospace;
+        padding-left: 50px;
+      }
+
+
+      table, th, td{
+        border: solid 1px;
+        text-align: left;
+      }
+      th{
+        vertical-align: top;
+      }
+
+    </style>
+  </head>
+
+
+<body>
+  <h1> Receipt </h1>
+
+    <table name="receipttable">
+      <tr>
+        <th>Your Name:</th>
+        <td><?php print $name; ?></td>
+      </tr>
+      <tr>
+        <th>Pay with:</th>
+        <td><?php print $payment;?></td>
+      </tr>
+      <tr>
+        <th>Date:</th>
+        <td><?php print(date("Y-m-d H:i:s"));?> </td>
+      </tr>
+      <tr>
+        <th>Order: </th>
+        <td>
+          <?php
+            if($apple!=0){
+              print $apple;
+              print(" <span>&#215;</span> apple <br>");
+            }
+            if($banana!=0){
+              print $banana;
+              print(" <span>&#215;</span> banana <br>");
+            }
+            if($orange!=0){
+              print $orange;
+              print(" <span>&#215;</span> orange <br>");
+            }
+         ?>
+      </td>
+      </tr>
+      <tr>
+        <th>Total Cost: </th>
+        <td><?php print("$");print $totalCost;?></td>
+      </tr>
+    </table>
+
+    <p align='center'>Thank you for ordering at The Fruit Shop. We hope to see you soon!</p>
 </body>
 </html>
